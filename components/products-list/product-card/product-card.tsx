a154fddff2ada949bd845188de85f751058ff873
Jona <mail2jona@yahoo.com>
2021-09-01 17:44:20 +0200

    added product list.

---
+++ components/products-list/product-card/product-card.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react'
+import IArticle from '../../../models/articles'
+import { IProduct } from '../../../models/products'
+import styles from './product-card.module.scss'
+
+export const ProductCard: FC<{ content: IProduct }> = ({ content }) => {
+	return (
+		<>
+			<div className={styles['wrapper']}>
+				<div className={styles['overviewInfo']}>
+					<div className={styles['productinfo']}>
+						<div className={styles['grouptext']}>
+							<h3>ID#</h3>
+							<p>{content.id}</p>
+						</div>
+					</div>
+				</div>
+
+				<div className={styles['productSpecifications']}>
+					<h1>{content.name}</h1>
+					<p className={styles['title']}>
+						Articles:&nbsp;{content.articles.length}
+					</p>
+				</div>
+			</div>
+		</>
+	)
+}
